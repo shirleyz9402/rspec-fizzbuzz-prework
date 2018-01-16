28ddb6c62ec0164e1cba99f1c8638555dc1418f3
Shirley Zhang <shirleyz9402@yahoo.com>
2018-01-16 20:39:09 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,10 +1,11 @@
 def fizzbuzz(num)
-  if (num % 3 == 0) && (num % 15 > 0)
+  if num % 15 == 0
+    return "FizzBuzz"
+  if num % 3 == 0
     return "Fizz"
   elsif num % 5 == 0
     return "Buzz"
-  elsif (num % 15 == 0)
-    return "FizzBuzz"
+ 
   else 
     return nil 
   end 
